5b0778922e037b27c2c4cfa8881701f18d324980
Paul Kim <pykim0591@gmail.com>
2019-02-11 15:08:53 -0700

    import games, teams csv to state

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,24 +1,23 @@
 require 'csv'
 
 class StatTracker
-  attr_reader :file_paths
+  attr_reader :file_paths, 
+              :teams, 
+              :games, 
+              :game_teams
 
   def initialize(file_paths)
     @file_paths = file_paths
   end
 
   def self.from_csv(files)
-    StatTracker.new(files)
+    new(files)
   end
-
 
   def load_csv
-    @file_paths.each do |keys, values|
-      x = CSV.read values
-      headers = x.shift.map {|header| header.to_sym}
-      body =
-x.map
-
-  end
+    @file_paths.each do |type, file_path|
+      data_set = CSV.open(file_path, headers: true, header_converters: :symbol)
+      instance_variable_set("@#{type}", data_set)
+    end
   end
 end
