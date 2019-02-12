49aa0b4655112d31d46d3f7f7d624572eacf8890
Paul Kim <pykim0591@gmail.com>
2019-02-12 10:15:05 -0700

    Add visitor and home methods

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -31,7 +31,7 @@ module LeagueStatistics
     team_goals_forfeited = total_goals_forfeited_by_team # from defensive_stats
     team_goals_forfeited.keys.min_by do |team_name|
       goals_forfeited = team_goals_forfeited[team_name]
-      (goals_forfeited.sum.to_f / goals_forfeited.count).round(2)
+      (goals_forfeited.sum.to_f / goals_forfeited.count).round(4)
     end
   end
 
@@ -39,7 +39,24 @@ module LeagueStatistics
     team_goals_forfeited = total_goals_forfeited_by_team # from defensive_stats
     team_goals_forfeited.keys.max_by do |team_name|
       goals_forfeited = team_goals_forfeited[team_name]
-      (goals_forfeited.sum.to_f / goals_forfeited.count).round(2)
+      (goals_forfeited.sum.to_f / goals_forfeited.count).round(4)
+    end
+  end
+
+  def highest_scoring_visitor
+    team_visitor_goals = total_goals_when_visiting
+    require 'pry'; binding.pry
+    team_visitor_goals.keys.max_by do |team_name|
+      visitor_goals = team_visitor_goals[team_name]
+      (visitor_goals.sum.to_f / visitor_goals.count).round(4)
+    end
+  end
+
+  def highest_scoring_home_team
+    team_home_goals = total_goals_when_at_home
+    team_home_goals.keys.max_by do |team_name|
+      home_goals = team_home_goals[team_name]
+      (home_goals.sum.to_f / home_goals.count).round(4)
     end
   end
 end
