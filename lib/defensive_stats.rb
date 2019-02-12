158b3a77cb2bd0f8d50651d444994c9617ba9f02
Paul Kim <pykim0591@gmail.com>
2019-02-12 09:37:35 -0700

    Create total_goals_forfeited method

--- lib/defensive_stats.rb
+++ lib/defensive_stats.rb
@@ -1,5 +1,20 @@
 module DefensiveStats
   def total_goals_forfeited_by_team
-    # @league.teams.inject({})
+    @league.teams.inject({}) do |teams_hash, team|
+      teams_hash[team.short_name] = []
+      @league.games.each do |game|
+        all_forfeited_goals = teams_hash[team.short_name]
+        if team.team_id == game.away_team_id
+          all_forfeited_goals << game.home_goals
+        elsif team.team_id == game.home_team_id
+          all_forfeited_goals << game.away_goals
+        else
+          next
+        end
+      end
+      forfeited_count = teams_hash[team.short_name].count
+      teams_hash.delete(team.short_name) if forfeited_count.zero?
+      teams_hash
+    end
   end
 end
