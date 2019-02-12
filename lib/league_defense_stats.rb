d90fdd071525f3631a11e4d78de0f3ea10ffdcc5
Paul Kim <pykim0591@gmail.com>
2019-02-12 11:52:10 -0700

    Change league subfile names

--- lib/league_defense_stats.rb
+++ lib/league_defense_stats.rb
@@ -1,9 +1,9 @@
-module DefensiveStats
+module LeagueDefenseStats
   def total_goals_forfeited_by_team
     @league.teams.inject({}) do |teams_hash, team|
-      teams_hash[team.short_name] = []
+      teams_hash[team.team_name] = []
       @league.games.each do |game|
-        all_forfeited_goals = teams_hash[team.short_name]
+        all_forfeited_goals = teams_hash[team.team_name]
         if team.team_id == game.away_team_id
           all_forfeited_goals << game.home_goals
         elsif team.team_id == game.home_team_id
@@ -12,8 +12,8 @@ module DefensiveStats
           next
         end
       end
-      forfeited_count = teams_hash[team.short_name].count
-      teams_hash.delete(team.short_name) if forfeited_count.zero?
+      forfeited_count = teams_hash[team.team_name].count
+      teams_hash.delete(team.team_name) if forfeited_count.zero?
       teams_hash
     end
   end
--- lib/league_offense_stats.rb
+++ lib/league_offense_stats.rb
@@ -1,9 +1,9 @@
-module OffensiveStats
+module LeagueOffenseStats
   def total_goals_made_by_team
     @league.teams.inject({}) do |teams_hash, team|
-      teams_hash[team.short_name] = []
+      teams_hash[team.team_name] = []
       @league.games.each do |game| 
-        team_goals = teams_hash[team.short_name]
+        team_goals = teams_hash[team.team_name]
         if team.team_id == game.away_team_id
           team_goals << game.away_goals
         elsif team.team_id == game.home_team_id
@@ -12,8 +12,6 @@ module OffensiveStats
           next
         end
       end
-      goals_count = teams_hash[team.short_name].count
-      teams_hash.delete(team.short_name) if goals_count.zero?
       teams_hash
     end
   end
@@ -23,8 +21,8 @@ module OffensiveStats
       visitor_games = @league.games.select do |game| 
         team.team_id == game.away_team_id
       end
-      visitor_goals = visitor_games.map { |game| game.away_goals }
-      teams_hash[team.short_name] = visitor_goals if !visitor_goals.count.zero?
+      gamevisitor_games.map {||}
+      teams_hash[team.team_name] = visitor_games
       teams_hash
     end
   end
@@ -34,8 +32,7 @@ module OffensiveStats
       home_games = @league.games.select do |game|
         game.home_team_id == team.team_id
       end
-      home_goals = home_games.map { |game| game.home_goals }
-      teams_hash[team.short_name] = home_goals if !home_goals.count.zero?
+      teams_hash[team.team_name] = home_games
       teams_hash
     end
   end
