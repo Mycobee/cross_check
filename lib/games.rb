b267022b1fc851cd962c7f666b5f639b41bb4e5e
Paul Kim <pykim0591@gmail.com>
2019-02-12 07:27:02 -0700

    Remove games ans teams classes

--- lib/games.rb
+++
@@ -1,9 +0,0 @@
-require './lib/game'
-
-class Games
-  attr_reader :game_list
-  
-  def initialize(game_list)
-    @game_list = game_list.map {|game_info| Game.new(game_info)}
-  end
-end
--- lib/teams_info.rb
+++
@@ -1,9 +0,0 @@
-require './lib/team'
-
-class TeamsInfo
-  attr_reader :teams_list
-  
-  def initialize(teams_list)
-    @teams_list = teams_list.map { |team| Team.new(team) }
-  end
-end
