561ce4d6c03bb681bb0955d7625eaff2f4d7e3fb
Ethan Grab <ethangrab@gmail.com>
2019-02-14 17:20:45 -0700

    team_statistics#favorite_opponent - in progress

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -179,16 +179,15 @@ class TrackerTest < Minitest::Test
 
   def test_best_season
     actual = @stat_tracker.best_season("3")
-    require 'pry'; binding.pry
-    
-    assert_equal 1,  actual
+
+    assert_equal 1, actual
   end
-  #
-  # def test_worst_season
-  #   skip
-  #   actual = @stat_tracker.worst_season
-  #   assert_equal #Integer, actual
-  # end
+
+  def test_worst_season
+    actual = @stat_tracker.worst_season("3")
+
+    assert_equal 1, actual
+  end
   #
   # def test_average_win_percenrage
   #   skip
@@ -211,12 +210,12 @@ class TrackerTest < Minitest::Test
   #
   # end
   #
-  # def test_favorite_opponent
-  #   skip
-  #   actual = @stat_tracker.favorite_opponent
-  #   assert_equal #String, actual
-  #
-  # end
+  def test_favorite_opponent
+
+    actual = @stat_tracker.favorite_opponent("3")
+    assert_equal 1, actual
+
+  end
   #
   # def test_rival
   #   skip
