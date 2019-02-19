4f1982c0df3746612930e395752aa6402b9f5c21
Ethan Grab <ethangrab@gmail.com>
2019-02-19 14:34:04 -0700

    LeagueDefenseStats#total_goals_forfeited_by_team refactor into two helper methods

--- lib/league_defense_stats.rb
+++ lib/league_defense_stats.rb
@@ -1,15 +1,24 @@
 module LeagueDefenseStats
+
+  def conditional_shoveling(team, game, all_forfeited_goals)
+    if team.team_id == game.away_team_id
+      all_forfeited_goals << game.home_goals
+    elsif team.team_id == game.home_team_id
+      all_forfeited_goals << game.away_goals
+    end
+  end
+
+  def goals_forfeited_by_team(teams_hash, team)
+    @league.games.each do |game|
+      all_forfeited_goals = teams_hash[team.team_id]
+      conditional_shoveling(team, game, all_forfeited_goals)
+    end
+  end
+
   def total_goals_forfeited_by_team
     @league.teams.inject({}) do |teams_hash, team|
       teams_hash[team.team_id] = []
-      @league.games.each do |game|
-        all_forfeited_goals = teams_hash[team.team_id]
-        if team.team_id == game.away_team_id
-          all_forfeited_goals << game.home_goals
-        elsif team.team_id == game.home_team_id
-          all_forfeited_goals << game.away_goals
-        end
-      end
+      goals_forfeited_by_team(teams_hash, team)
       forfeited_count = teams_hash[team.team_id].count
       teams_hash.delete(team.team_id) if forfeited_count.zero?
       teams_hash
