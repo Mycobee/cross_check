8dcec8f5fe8751343226760d71032715fe633e45
Ethan Grab <ethangrab@gmail.com>
2019-02-19 14:59:08 -0700

    Refactored league_offense_stats#home_goals,#away_goals,#total_goals_when_visiting,#total_goals_when_at_home

--- lib/league_offense_stats.rb
+++ lib/league_offense_stats.rb
@@ -1,23 +1,17 @@
 module LeagueOffenseStats
 
-  def away_goals(team_id)
-    away_games = @league.games.select do |game|
-      team_id == game.away_team_id
-    end
-    away_games.map {|game| game.away_goals}
-  end
 
-  def home_goals(team_id)
-    home_games = @league.games.select do |game|
-      team_id == game.home_team_id
+  def home_away_goals(team_id, i_var1, i_var2)
+    home_away_goal_team = @league.games.select do |game|
+      team_id == game.send(i_var1.to_sym)
     end
-    home_games.map {|game| game.home_goals}
+    home_away_goal_team.map {|game| game.send(i_var2.to_sym)}
   end
 
   def total_goals_made_by_team
    @league.teams.inject({}) do |teams_hash, team|
-    away_game_goals = away_goals(team.team_id)
-    home_game_goals = home_goals(team.team_id)
+    away_game_goals = home_away_goals(team.team_id, "away_team_id", "away_goals")
+    home_game_goals = home_away_goals(team.team_id, "home_team_id", "home_goals")
     if !away_game_goals.length.zero? || !home_game_goals.length.zero?
       teams_hash[team.team_id] = away_game_goals + home_game_goals
     end
@@ -25,25 +19,19 @@ module LeagueOffenseStats
     end
   end
 
-  def total_goals_when_visiting
+  def home_away_goals_scored(i_var1, i_var2)
     @league.teams.inject({}) do |teams_hash, team|
-      visitor_games = @league.games.select do |game| 
-        team.team_id == game.away_team_id
-      end
-      visitor_goals = visitor_games.map {|game| game.away_goals}
-      teams_hash[team.team_id] = visitor_goals if !visitor_goals.count.zero?
+      home_away_goals = home_away_goals(team.team_id, i_var1, i_var2)
+      teams_hash[team.team_id] = home_away_goals if !home_away_goals.count.zero?
       teams_hash
     end
   end
 
-  def total_goals_when_at_home
-    @league.teams.inject({}) do |teams_hash, team|
-      home_games = @league.games.select do |game|
-        game.home_team_id == team.team_id
-      end
-      home_goals = home_games.map {|game| game.home_goals}
-      teams_hash[team.team_id] = home_goals if !home_goals.count.zero?
-      teams_hash
-    end
+  def total_goals_when_visiting
+    home_away_goals_scored("away_team_id", "away_goals")
   end
+
+  def total_goals_when_at_home
+    home_away_goals_scored("home_team_id", "home_goals")
+  end 
 end
